961b2e50f6e8f305ec080ab37e854b9790049eb0
fl1p12 <colinpiano@icloud.com>
2025-10-12 20:16:34 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,15 @@
+import pyautogui
+import time
+import keyboard
+
+num_goal = 50
+
+time.sleep(5)
+
+for i in range(num_goal):
+    pyautogui.write(str(i+1))
+    pyautogui.press("backspace",len(str(i+1)))
+    if keyboard.is_pressed("x"):
+        print("broke")
+        break
+#down
