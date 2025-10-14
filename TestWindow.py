0a6d34a63194bddc2ee4d1ba1423d4d4223b2d94
fl1p12 <colinpiano@icloud.com>
2025-10-14 17:40:45 -0400

    Add files via upload

---
+++ TestWindow.py
@@ -0,0 +1,49 @@
+import tkinter as tk
+import time
+
+numtocountto = None
+currentnum = 0
+
+window = tk.Tk()
+window.title("Count")
+window.geometry("400x320")
+window.resizable(False,False)
+
+label = tk.Label(text="Enter Number to count to",height=2,width=50)
+label.pack()
+
+text_box = tk.Text(width=30,height=7)
+text_box.pack()
+
+def buttonpressed(event):
+    global numtocountto
+    numtocountto = int(text_box.get("1.0",tk.END))
+
+    window.destroy()
+
+button = tk.Button(text="Enter",relief="raised")
+button.bind("<Button-1>",buttonpressed)
+button.pack()
+
+window.mainloop()
+
+DisplayWindow = tk.Tk()
+DisplayWindow.title("Now it counts")
+DisplayWindow.geometry("400x150")
+DisplayWindow.resizable(False,False)
+
+DisplayNum = tk.Label(DisplayWindow,text=f'Count to this bitch: {numtocountto}')
+DisplayNum.pack()
+
+DisplayText = tk.Label(DisplayWindow,height=5,width=30,font=("Arial",25),text=0)
+DisplayText.pack()
+
+def count(event):
+    global currentnum
+    #if currentnum < numtocountto:
+    currentnum += 1
+    DisplayText.config(text=currentnum)
+
+DisplayWindow.bind('<Button-1>',count)
+
+DisplayWindow.mainloop()
